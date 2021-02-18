11cb87b2f547a742f0acd3642f2bfa3ff4459be0
Kelly Tong <kellytong@Kellys-MacBook-Air.local>
2021-02-17 21:31:52 -0800

    calculate proportion of leading offense

--- summary_info.R
+++ summary_info.R
@@ -7,7 +7,18 @@ rates_by_race <- read.csv("data/arrest_rates_by_race.csv", check.names = FALSE)
 rates_of_resi_pl<- read.csv(file = "data/Juvenile Residential Placement.csv")
 
 # Calculate the proportion of juvenile offenses compared to all ages offenses
-prop_of_juveniles <- arrests %>% 
+all_offenses <- arrests %>% 
+  filter(`0 to 17` == max(`0 to 17`, na.rm = T)) %>% 
+  pull(`0 to 17`)
+
+top_3 <- arrests %>% 
+  top_n(3, wt = `0 to 17`) %>% 
+  arrange(-`0 to 17`)
+
+leading_offense <- top_3[-c(1, 2),] %>% 
+  pull(`0 to 17`)
+
+prop_of_juveniles <- paste0(round((leading_offense / all_offenses) * 100, digits = 2), "%")
   
   
 
