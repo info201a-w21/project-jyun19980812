3b997d7f6d02931e8018e60bf420d9a19dbb01a4
Casey Mang <cmang@uw.edu>
2021-03-06 22:19:12 -0800

    Added pages for shiny app

--- juvenile_app/app_server.R
+++ juvenile_app/app_server.R
@@ -1,4 +1,13 @@
 # This is app_server.R file.
+library(shiny)
+library(tidyverse)
+library(maps)
+library(plotly)
+
+# Load the data
+rates_of_resi_pl<- read.csv(file = "data/Juvenile Residential Placement.csv")
+arrests <- read.csv("data/juv_offenses.csv", stringsAsFactors = F, check.names = F)
+rates_by_race <- read.csv("data/arrest_rates_by_race.csv", check.names = F)
 
 server <- function(input, output) {
   
--- juvenile_app/app_ui.R
+++ juvenile_app/app_ui.R
@@ -1,4 +1,37 @@
 # This is app_ui.R file.
 
+library(shiny)
+
+intro_panel <- tabPanel(
+  "Introduction",
+  h1("Introduction")
+)
+
+interactive1_panel <- tabPanel(
+  "Interactive 1",
+  h1("Kelly's Interactive")
+)
+
+interactive2_panel <- tabPanel(
+  "Interactive 2",
+  h1("Casey's Interactive")
+)
+
+interactive3_panel <- tabPanel(
+  "Interactive 3",
+  h1("Ryan's Interactive")
+)
+
+summary_panel <- tabPanel(
+  "Summary",
+  h1("Summary")
+)
+
 ui <- navbarPage(
+  "Juvenile Incarceration",
+  intro_panel,
+  interactive1_panel,
+  interactive2_panel,
+  interactive3_panel,
+  summary_panel
 )
